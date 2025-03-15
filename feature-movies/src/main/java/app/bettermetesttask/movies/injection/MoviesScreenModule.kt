c502ed47f2740e15c04d37ef2650db4daa17365f
Anton Artim <artimanton1987@gmail.com>
2025-03-15 17:39:27 +0300

    implemented MoviesScreenModule

--- feature-movies/src/main/java/app/bettermetesttask/movies/injection/MoviesScreenModule.kt
+++ feature-movies/src/main/java/app/bettermetesttask/movies/injection/MoviesScreenModule.kt
@@ -1,9 +1,52 @@
 package app.bettermetesttask.movies.injection
 
+import app.bettermetesttask.domainmovies.interactors.AddMovieToFavoritesUseCase
+import app.bettermetesttask.domainmovies.interactors.GetMovieUseCase
+import app.bettermetesttask.domainmovies.interactors.ObserveMoviesUseCase
+import app.bettermetesttask.domainmovies.interactors.RemoveMovieFromFavoritesUseCase
+import app.bettermetesttask.featurecommon.injection.scopes.FragmentScope
+import app.bettermetesttask.movies.sections.MoviesAdapter
+import app.bettermetesttask.movies.sections.MoviesViewModel
+import app.bettermetesttask.movies.sections.compose.MovieDetailsViewModel
 import dagger.Module
+import dagger.Provides
 
 @Module
 class MoviesScreenModule {
 
+    @Provides
+    @FragmentScope
+    fun provideMoviesViewModel(
+        observeMoviesUseCase: ObserveMoviesUseCase,
+        addMovieToFavoritesUseCase: AddMovieToFavoritesUseCase,
+        removeMovieFromFavoritesUseCase: RemoveMovieFromFavoritesUseCase,
+        adapter: MoviesAdapter
+    ): MoviesViewModel {
+        return MoviesViewModel(
+            observeMoviesUseCase,
+            addMovieToFavoritesUseCase,
+            removeMovieFromFavoritesUseCase,
+            adapter
+        )
+    }
 
+    @Provides
+    @FragmentScope
+    fun provideMovieDetailsViewModel(
+        getMovieUseCase: GetMovieUseCase,
+        addMovieToFavoritesUseCase: AddMovieToFavoritesUseCase,
+        removeMovieFromFavoritesUseCase: RemoveMovieFromFavoritesUseCase
+    ): MovieDetailsViewModel {
+        return MovieDetailsViewModel(
+            getMovieUseCase,
+            addMovieToFavoritesUseCase,
+            removeMovieFromFavoritesUseCase
+        )
+    }
+
+    @Provides
+    @FragmentScope
+    fun provideMoviesAdapter(): MoviesAdapter {
+        return MoviesAdapter()
+    }
 }
